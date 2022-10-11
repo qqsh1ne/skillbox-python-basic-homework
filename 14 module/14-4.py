943ed552733a366a6608034f47c79e0887967fb1
qqsh1ne <qqsh1neee@gmail.com>
2022-10-11 11:21:03 +0500

    hw14 completed

--- 14 module/14-4.py
+++ 14 module/14-4.py
@@ -1,11 +1,25 @@
-import math
-
-
-def reverse_number(number):
+def reverse_float_number(number):
     int_part = int(number)
+    float_part_len = len(str(number)) - len(str(int(number))) - 1
+    float_part = int(number * 10 ** float_part_len % 10 ** float_part_len)
+
+    return float(str(reverse_int_number(int_part)) + '.' + str(reverse_int_number(float_part)))
+
+
+def reverse_int_number(number):
+    result = ''
+    while number > 0:
+        result += str(number % 10)
+        number //= 10
+    return int(result)
 
 
 first_number = float(input('Введите первое число: '))
 second_number = float(input('Введите второе число: '))
 
-print(math.fmod(first_number, 1))
+first_number_reversed = reverse_float_number(first_number)
+second_number_reversed = reverse_float_number(second_number)
+
+print('Первое число наоборот:', first_number_reversed)
+print('Второе число наоборот:', second_number_reversed)
+print('Сумма:', first_number_reversed + second_number_reversed)
